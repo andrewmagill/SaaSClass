2fa1472f8b6a09968e5a144fa6a92a1fa8139d64
Andrew Magill <andrewbmagill@yahoo.com>
2013-07-27 16:26:43 -0500

    part three done

--- hw1/part3.rb
+++ hw1/part3.rb
@@ -1,3 +1,11 @@
+def combine_anagrams_not_accepted(words)
+  hash = {}
+  words.each do |word|
+    key = word.downcase.chars.sort.join
+    hash.has_key?(key) ? hash[key] << word : hash[key] = [word] 
+  end
+end
+
 def combine_anagrams(words)
- # YOUR CODE HERE
+  words.group_by{|w| w.downcase.chars.sort.join}.values
 end
