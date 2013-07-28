afd1cf6b9b524ded878e6cd51b9e9c1611a74eb2
Andrew Magill <andrewbmagill@yahoo.com>
2013-07-28 15:14:02 -0500

    part seven done

--- hw1/part7.rb
+++ hw1/part7.rb
@@ -1,4 +1,11 @@
 class CartesianProduct
   include Enumerable
-  # YOUR CODE HERE
+  @seq_a = []
+  @seq_b = []
+  def initialize(seq_a, seq_b)
+    @seq_a, @seq_b = seq_a, seq_b
+  end 
+  def each(&block)
+    @seq_a.product(@seq_b).each(&block)
+  end
 end
